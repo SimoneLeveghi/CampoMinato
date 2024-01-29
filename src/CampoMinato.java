f7c0cfa440afa3e593f126b2b48b3272dd9a7519
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-01-29 08:36:01 +0100

    Implemented methods inizializzaCampo() and posizionaMine()

--- src/CampoMinato.java
+++ src/CampoMinato.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Random;
+
 public class CampoMinato {
     int[][] campo;
 
@@ -5,5 +8,32 @@ public class CampoMinato {
         this.campo = new int[righe][colonne];
     }
 
-    
+    public void inizializzaCampo() {
+        for(int i = 0; i < this.campo.length; ++i) {
+            for(int j = 0; j < this.campo[0].length; ++j) {
+                this.campo[i][j] = 0;
+            }
+        }
+    }
+
+    public void posizionaMine(int nMine) {
+        LinkedList<Integer> celleDisponibili = new LinkedList<Integer>();
+        for(int i = 0; i < campo.length * campo[0].length; ++i) {
+            celleDisponibili.add(i);
+        }
+
+        Random r = new Random();
+
+        for(int i = 0; i < nMine; ++i) {
+            int cella = r.nextInt(0, celleDisponibili.size());
+            celleDisponibili.remove(cella);
+
+            int riga = cella / this.campo[0].length;
+            int colonna = cella % this.campo[0].length;
+
+            this.campo[riga][colonna] = -1;
+        }
+    }
+
+
 }
